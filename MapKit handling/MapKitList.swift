5623e3339b0203a336f238d5a5468108356fccd3
Taehyeon Han <calmone@omnicns.com>
2018-09-12 15:51:31 +0900

    Add - Image to annotation

--- MapKit handling/MapKitList.swift
+++ MapKit handling/MapKitList.swift
@@ -15,10 +15,6 @@ enum MapKitList: Int {
     case _04
     case _05
     case _06
-    
-    /* Don't use */
-    case COUNT
-    
     case _07
     case _08
     case _09
@@ -30,6 +26,9 @@ enum MapKitList: Int {
     case _15
     case _16
     
+    /* Don't use */
+    case COUNT
+    
     func getInfo() -> ListInfo {
         switch self {
         case ._01: return ListInfo(name: "Display map")
@@ -38,16 +37,16 @@ enum MapKitList: Int {
         case ._04: return ListInfo(name: "Pin up")
         case ._05: return ListInfo(name: "3D display")
         case ._06: return ListInfo(name: "Drop the pin")
-        case ._07: return ListInfo(name: "Display Map")
-        case ._08: return ListInfo(name: "Display Map")
-        case ._09: return ListInfo(name: "Display Map")
-        case ._10: return ListInfo(name: "Display Map")
-        case ._11: return ListInfo(name: "Display Map")
-        case ._12: return ListInfo(name: "Display Map")
-        case ._13: return ListInfo(name: "Display Map")
-        case ._14: return ListInfo(name: "Display Map")
-        case ._15: return ListInfo(name: "Display Map")
-        case ._16: return ListInfo(name: "Display Map")
+        case ._07: return ListInfo(name: "Image to annotation")
+        case ._08: return ListInfo(name: "arbitrary image")
+        case ._09: return ListInfo(name: "comming soon...")
+        case ._10: return ListInfo(name: "comming soon...")
+        case ._11: return ListInfo(name: "comming soon...")
+        case ._12: return ListInfo(name: "comming soon...")
+        case ._13: return ListInfo(name: "comming soon...")
+        case ._14: return ListInfo(name: "comming soon...")
+        case ._15: return ListInfo(name: "comming soon...")
+        case ._16: return ListInfo(name: "comming soon...")
             
             /* Don't use */
         case .COUNT: return ListInfo(name: "Count")
--- MapKit handling/ViewController/_07.swift
+++ MapKit handling/ViewController/_07.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
+import MapKit
 
 class _07: BaseViewController {
 
+    var center: CLLocationCoordinate2D!
+
+    // Generate MKMapView
+    lazy var _mapView: MKMapView = {
+        let mv = MKMapView(frame: self.view.frame)
+        
+        mv.centerCoordinate = center
+        mv.delegate = self
+        
+        // Specify the scale.
+        let mySpan: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let myRegion: MKCoordinateRegion = MKCoordinateRegion(center: center, span: mySpan)
+        
+        // Added region to mapView.
+        mv.region = myRegion
+        
+        // viewにmapViewを追加.
+        self.view.addSubview(mv)
+        
+        // Draw a circle (radius 500 m).
+        // let myCircle: MKCircle = MKCircle(centerCoordinate: center, radius: CLLocationDistance(500))
+        let myCircle: MKCircle = MKCircle(center: center, radius: CLLocationDistance(500))
+        
+        // Add circle to mapView.
+        mv.add(myCircle)
+        
+        // Generate an annotation pointing to center.
+        let myPointAnnotation: MKPointAnnotation = MKPointAnnotation()
+        myPointAnnotation.title = "title"
+        myPointAnnotation.subtitle = "subtitle"
+        myPointAnnotation.coordinate = center
+        
+        // Added annotation to mapView.
+        mv.addAnnotation(myPointAnnotation)
+        
+        return mv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        center = CLLocationCoordinate2DMake(kDefault_latitude, kDefault_longitude)
+        
         // Do any additional setup after loading the view.
+        
+        // Add mapView on view
+        self.view.addSubview(_mapView)
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -21,4 +66,49 @@ class _07: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension _07: MKMapViewDelegate {
+    
+    // Delegate method called when addAnnotation is done.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // Identifier is generated.
+        let myAnnotationIdentifier: String = "myAnnotation"
+        
+        // Annotation generation.
+        var myAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: myAnnotationIdentifier)
+        
+        if myAnnotationView == nil {
+            
+            myAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: myAnnotationIdentifier)
+            
+            // Allow annotation callouts.
+            myAnnotationView!.canShowCallout = true
+            
+            // Add an image to the annotation.
+            myAnnotationView!.leftCalloutAccessoryView = UIImageView(image: UIImage(named: "logo.jpeg"))
+        }
+        
+        return myAnnotationView
+    }
+    
+    // Delegate method called when addOverlay is done.
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        // // Generate renderer.
+        let myCircleView: MKCircleRenderer = MKCircleRenderer(overlay: overlay)
+        
+        // Fill the interior of the circle with blue.
+        myCircleView.fillColor = UIColor.blue
+        
+        // Transmit the circle.
+        myCircleView.alpha = 0.5
+        
+        // Thickness of the circumference line.
+        myCircleView.lineWidth = 0.5
+        
+        return myCircleView
+    }
+    
 }
